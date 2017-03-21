ea28fb42002b92ea705033bb96f7b573da0094b1
oneraul <oneraul@gmail.com>
2017-03-21 14:39:12 +0100

    Added arrow keys for player movement

--- player.lua
+++ player.lua
@@ -55,10 +55,10 @@ end
 
 function Player:movementAndCollision(dt)
 	local dx, dy = 0, 0
-	if love.keyboard.isDown("w") then dy = dy - 1 end
-	if love.keyboard.isDown("a") then dx = dx - 1 end
-	if love.keyboard.isDown("s") then dy = dy + 1 end
-	if love.keyboard.isDown("d") then dx = dx + 1 end
+	if love.keyboard.isDown("w", "up") then dy = dy - 1 end
+	if love.keyboard.isDown("a", "left") then dx = dx - 1 end
+	if love.keyboard.isDown("s", "down") then dy = dy + 1 end
+	if love.keyboard.isDown("d", "right") then dx = dx + 1 end
 	
 	if dy ~= 0 then
 		self.y = self.y + dy * v * dt
